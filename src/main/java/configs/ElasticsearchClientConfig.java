195bb6f6dd770a892ebd2a10d7c1c5c80b22e464
ali <aallii.r98@gmail.com>
2023-04-11 00:14:47 +0430

    add elasticsearch

---
+++ src/main/java/configs/ElasticsearchClientConfig.java
@@ -0,0 +1,27 @@
+package configs;
+
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.erhlc.RestClients;
+
+@Configuration
+public class ElasticsearchClientConfig {
+
+    @Value("${elasticsearch.host}")
+    private String host;
+
+    @Value("${elasticsearch.port}")
+    private int port;
+
+    @Bean
+    public RestHighLevelClient client() {
+        final ClientConfiguration configuration = ClientConfiguration.builder()
+                .connectedTo(host + ":" + port)
+                .build();
+
+        return RestClients.create(configuration).rest();
+    }
+}
