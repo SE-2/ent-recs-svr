179bea54f4bac318e2d38809010a9d8a9a43da91
ali <aallii.r98@gmail.com>
2023-05-02 23:34:08 +0430

    entity and controller sample added

--- src/main/java/backend/main/model/entity/User.java
+++ src/main/java/backend/main/model/entity/User.java
@@ -1,5 +1,24 @@
 package backend.main.model.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
 
+import java.util.Set;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "User")
+@Entity
+@Builder
 public class User {
+    @Id
+    private String ID;
+    @Column
+    private String Email;
+    @Column
+    private String Token;
+    @Column
+    private String BirthDate;
 }
