85415f1181dda0b74770016a3eb2a5bff89a6ebf
Mohammad <tavakolimohammad969@gmail.com>
2023-05-04 19:29:49 +0330

    add repository and service for FavPodcastProducer

---
+++ src/main/java/backend/main/repository/FavoritePodcastProducerRepository.java
@@ -0,0 +1,9 @@
+package backend.main.repository;
+
+import backend.main.model.entity.*;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FavoritePodcastProducerRepository extends CrudRepository<FavoritePodcastProducer, String>  {
+}
