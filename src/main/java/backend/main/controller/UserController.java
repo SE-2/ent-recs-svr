fc3f62a7370ba0c4592fa94efa3d0c4a086144ab
ali <aallii.r98@gmail.com>
2023-06-04 11:46:03 +0330

    add get user api

--- src/main/java/backend/main/controller/UserController.java
+++ src/main/java/backend/main/controller/UserController.java
@@ -1,7 +1,8 @@
 package backend.main.controller;
 
 import backend.main.business.interfaces.service.IUserService;
-import backend.main.model.dto.UserAddDto;
+import backend.main.model.dto.*;
+import backend.main.model.entity.User;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,5 +22,11 @@ public class UserController {
         } catch (Exception e) {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
         }
+    }
+
+    @GetMapping("/getUser")
+    public ResponseEntity<UserGetDto> getUser(@RequestHeader("Token") String token) {
+        UserGetDto user = userService.getUser(token);
+        return new ResponseEntity<>(user, HttpStatus.CREATED);
     }
 }
