580a2ed64fc3bcdb0ef8c2602dc16652221916cc
Mohammad <tavakolimohammad969@gmail.com>
2023-06-04 12:54:22 +0330

    show trends

--- src/main/java/backend/main/business/implementation/service/trend/TrendMediaService.java
+++ src/main/java/backend/main/business/implementation/service/trend/TrendMediaService.java
@@ -10,7 +10,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -25,39 +24,11 @@ public class TrendMediaService implements ITrendMediaService {
 
     @Override
     public List<MediaMetadata> getTodayTrendMedia() {
-        LocalDate today = LocalDate.now();
-        List<Like> likes = likeRepository.findAllByDate(today.toString());
-
-        mediaSortService.sort(likes,SortMethod.POPULARITY);
-
-        // Sort media by like count
-        List<MediaMetadata> sortedMedia = likesCountMap.entrySet().stream()
-                .sorted(Map.Entry.<String, Integer>comparingByValue().reversed())
-                .map(entry -> mediaSortService.getMediaById(entry.getKey()))
-                .collect(Collectors.toList());
-
-        return sortedMedia;
+        return new ArrayList<>();
     }
 
     @Override
     public List<MediaMetadata> getAllTimeTrendMedia() {
-        // Get all media metadata
-        List<MediaMetadata> allMedia = mediaSortService.getAllMedia();
-
-        // Map media IDs to their like counts
-        Map<String, Integer> likesCountMap = new HashMap<>();
-        for (MediaMetadata media : allMedia) {
-            String mediaId = media.getMediaId();
-            int count = likeRepository.countByMediaId(mediaId);
-            likesCountMap.put(mediaId, count);
-        }
-
-        // Sort media by like count
-        List<MediaMetadata> sortedMedia = likesCountMap.entrySet().stream()
-                .sorted(Map.Entry.<String, Integer>comparingByValue().reversed())
-                .map(entry -> mediaSortService.getMediaById(entry.getKey()))
-                .collect(Collectors.toList());
-
-        return sortedMedia;
+        return new ArrayList<>();
     }
 }
