f0e30ef9e9fad700869c4c638c3ffb9908aa76a2
ali <aallii.r98@gmail.com>
2023-06-05 12:00:32 +0330

    fix result bug

--- src/main/java/backend/main/business/implementation/updater/FavoriteMusicUpdater.java
+++ src/main/java/backend/main/business/implementation/updater/FavoriteMusicUpdater.java
@@ -18,7 +18,8 @@ public class FavoriteMusicUpdater implements IFavoriteMusicUpdater {
     @Override
     public void updateMusicFavorites(List<Music> musics, User user) {
         List<String> allSingers = new ArrayList<>();
-        for (Music music : musics) {
+        for (int i = 0; i < 3; i++) {
+            Music music = musics.get(i);
             allSingers.add(music.getArtist());
         }
 
